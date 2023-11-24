7736df3178a62840b8ab66d4feadad8e6a1bf09c
IrinaJ <irina.ul87@gmail.com>
2023-11-24 18:15:08 -0500

    Browse/filtering added

--- item/views.py
+++ item/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
+
+
+def items(request):
+  query = request.GET.get('query', '')
+  items = Item.objects.filter(is_sold=False)
+  categories = Category.objects.all()
+  category_id = request.GET.get('category', 0)
+  
+  if query:
+    items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+  if category_id:
+    items = items.filter(category_id=category_id)
+
+  return render(request, 'item/items.html', {
+    'items': items,
+    'query': query,
+    'categories': categories, 
+    'category_id': int(category_id)
+  })
 
 def detail(request, pk):
   item = get_object_or_404(Item, pk=pk)
