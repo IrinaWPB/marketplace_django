99b4244cf99a5e07a9e343f0e840e906c2ff2b75
IrinaJ <irina.ul87@gmail.com>
2023-11-26 18:23:17 -0500

    messaging

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'core',
     'item',
     'dashboard',
+    'conversation',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# TEMPLATE_DIRS = (
+#     os.path.join(SETTINGS_PATH, 'templates')
+# )
